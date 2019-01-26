b295b41974fa7406dff480ab343ac98617e12083
youxkei <youxkei@gmail.com>
2019-01-26 20:53:11 +0900

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,8 @@
 #![feature(stmt_expr_attributes)]
 extern crate erl_tokenize;
 
+#[cfg(test)]
+extern crate insta;
+
 pub mod layout_expr;
 pub mod token;
--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+use insta::assert_debug_snapshot_matches;
+
 #[derive(Clone, Copy, Debug)]
 pub struct Position {
     pub offset: usize,
@@ -6,12 +9,18 @@ pub struct Position {
 }
 
 impl Position {
-    fn forward(self: &mut Self, n: usize) {
+    fn forward(&mut self, n: usize) {
         self.offset += n;
         self.column += n;
     }
+
+    fn break_line(&mut self) {
+        self.column = 1;
+        self.line += 1;
+    }
 }
 
+#[derive(Debug)]
 pub enum Symbol {
     OpenParen,
     CloseParen,
@@ -26,7 +35,7 @@ pub enum Symbol {
     LessHyphen,
     Semicolon,
     Colon,
-    Numbersign,
+    NumberSign,
     Dot,
     Asterisk,
     Slash,
@@ -52,10 +61,11 @@ pub enum Symbol {
     ColonColon,
     DotDot,
     DotDotDot,
-    DotNewLine,
     Question,
+    NewLine,
 }
 
+#[derive(Debug)]
 pub enum Keyword {
     After,
     Begin,
@@ -84,6 +94,7 @@ pub enum Keyword {
     Xor,
 }
 
+#[derive(Debug)]
 pub enum TokenType {
     Integer,
     Float,
@@ -96,12 +107,14 @@ pub enum TokenType {
     Whitespace,
 }
 
+#[derive(Debug)]
 pub struct Token<'a> {
     pub text: &'a [u8],
     pub position: Position,
     pub token_type: TokenType,
 }
 
+#[derive(Debug)]
 pub enum Error {}
 
 pub struct Tokenizer<'a> {
@@ -117,11 +130,21 @@ impl<'a> Tokenizer<'a> {
         }
     }
 
-    fn tokenize_symbol(&mut self, length: usize, symbol: Symbol, rest: &'a [u8]) -> Option<Result<Token<'a>, Error>>{
+    fn tokenize_symbol(&mut self, length: usize, symbol: Symbol, rest: &'a [u8]) -> Option<Result<Token<'a>, Error>> {
+        let src = self.src;
         let position = self.current_position;
+
         self.src = rest;
         self.current_position.forward(length);
-        Some(Ok(Token { text: &self.src[0..length], position, token_type: TokenType::Symbol(symbol) }))
+        if let Symbol::NewLine = symbol {
+            self.current_position.break_line();
+        }
+
+        Some(Ok(Token {
+            text: &src[0..length],
+            position,
+            token_type: TokenType::Symbol(symbol),
+        }))
     }
 }
 
@@ -129,120 +152,174 @@ impl<'a> Iterator for Tokenizer<'a> {
     type Item = Result<Token<'a>, Error>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self.src {
-            &[] => None,
+        loop {
+            match self.src {
+                &[] => return None,
 
-            &[b'=', b':', b'=', ref rest..] => self.tokenize_symbol(3, Symbol::EqualColonEqual, rest),
-            &[b'=', b'/', b'=', ref rest..] => self.tokenize_symbol(3, Symbol::EqualSlashEqual, rest),
-            &[b'.', b'.', b'.', ref rest..] => self.tokenize_symbol(3, Symbol::DotDotDot, rest),
+                &[b'=', b':', b'=', ref rest..] => return self.tokenize_symbol(3, Symbol::EqualColonEqual, rest),
+                &[b'=', b'/', b'=', ref rest..] => return self.tokenize_symbol(3, Symbol::EqualSlashEqual, rest),
+                &[b'.', b'.', b'.', ref rest..] => return self.tokenize_symbol(3, Symbol::DotDotDot, rest),
 
-            &[b'-', b'>', ref rest..] => self.tokenize_symbol(2, Symbol::HyphenGreater, rest),
-            &[b'|', b'|', ref rest..] => self.tokenize_symbol(2, Symbol::BarBar, rest),
-            &[b'<', b'-', ref rest..] => self.tokenize_symbol(2, Symbol::LessHyphen, rest),
-            &[b'+', b'+', ref rest..] => self.tokenize_symbol(2, Symbol::PlusPlus, rest),
-            &[b'-', b'-', ref rest..] => self.tokenize_symbol(2, Symbol::HyphenHyphen, rest),
-            &[b'=', b'=', ref rest..] => self.tokenize_symbol(2, Symbol::EqualEqual, rest),
-            &[b'/', b'=', ref rest..] => self.tokenize_symbol(2, Symbol::SlashEqual, rest),
-            &[b'=', b'<', ref rest..] => self.tokenize_symbol(2, Symbol::SlashEqual, rest),
-            &[b'>', b'=', ref rest..] => self.tokenize_symbol(2, Symbol::GreaterEqual, rest),
-            &[b'<', b'=', ref rest..] => self.tokenize_symbol(2, Symbol::LessEqual, rest),
-            &[b'=', b'>', ref rest..] => self.tokenize_symbol(2, Symbol::EqualGreater, rest),
-            &[b':', b'=', ref rest..] => self.tokenize_symbol(2, Symbol::ColonEqual, rest),
-            &[b'.', b'.', ref rest..] => self.tokenize_symbol(2, Symbol::DotDot, rest),
+                &[b'-', b'>', ref rest..] => return self.tokenize_symbol(2, Symbol::HyphenGreater, rest),
+                &[b'|', b'|', ref rest..] => return self.tokenize_symbol(2, Symbol::BarBar, rest),
+                &[b'<', b'-', ref rest..] => return self.tokenize_symbol(2, Symbol::LessHyphen, rest),
+                &[b'+', b'+', ref rest..] => return self.tokenize_symbol(2, Symbol::PlusPlus, rest),
+                &[b'-', b'-', ref rest..] => return self.tokenize_symbol(2, Symbol::HyphenHyphen, rest),
+                &[b'=', b'=', ref rest..] => return self.tokenize_symbol(2, Symbol::EqualEqual, rest),
+                &[b'/', b'=', ref rest..] => return self.tokenize_symbol(2, Symbol::SlashEqual, rest),
+                &[b'=', b'<', ref rest..] => return self.tokenize_symbol(2, Symbol::EqualLess, rest),
+                &[b'>', b'=', ref rest..] => return self.tokenize_symbol(2, Symbol::GreaterEqual, rest),
+                &[b'<', b'=', ref rest..] => return self.tokenize_symbol(2, Symbol::LessEqual, rest),
+                &[b'=', b'>', ref rest..] => return self.tokenize_symbol(2, Symbol::EqualGreater, rest),
+                &[b':', b'=', ref rest..] => return self.tokenize_symbol(2, Symbol::ColonEqual, rest),
+                &[b'<', b'<', ref rest..] => return self.tokenize_symbol(2, Symbol::LessLess, rest),
+                &[b'>', b'>', ref rest..] => return self.tokenize_symbol(2, Symbol::GreaterGreater, rest),
+                &[b':', b':', ref rest..] => return self.tokenize_symbol(2, Symbol::ColonColon, rest),
+                &[b'.', b'.', ref rest..] => return self.tokenize_symbol(2, Symbol::DotDot, rest),
+                &[b'\r', b'\n', ref rest..] => return self.tokenize_symbol(2, Symbol::NewLine, rest),
 
-            &[b'(', ref rest..] => self.tokenize_symbol(1, Symbol::OpenParen, rest),
-            &[b')', ref rest..] => self.tokenize_symbol(1, Symbol::CloseParen, rest),
-            &[b',', ref rest..] => self.tokenize_symbol(1, Symbol::Comma, rest),
-            &[b'{', ref rest..] => self.tokenize_symbol(1, Symbol::OpenBrace, rest),
-            &[b'}', ref rest..] => self.tokenize_symbol(1, Symbol::CloseBrace, rest),
-            &[b'[', ref rest..] => self.tokenize_symbol(1, Symbol::OpenBracket, rest),
-            &[b']', ref rest..] => self.tokenize_symbol(1, Symbol::CloseBracket, rest),
-            &[b'|', ref rest..] => self.tokenize_symbol(1, Symbol::Bar, rest),
-            &[b';', ref rest..] => self.tokenize_symbol(1, Symbol::Semicolon, rest),
-            &[b':', ref rest..] => self.tokenize_symbol(1, Symbol::Colon, rest),
-            &[b'#', ref rest..] => self.tokenize_symbol(1, Symbol::Numbersign, rest),
-            &[b'.', ref rest..] => self.tokenize_symbol(1, Symbol::Dot, rest),
-            &[b'*', ref rest..] => self.tokenize_symbol(1, Symbol::Asterisk, rest),
-            &[b'/', ref rest..] => self.tokenize_symbol(1, Symbol::Slash, rest),
-            &[b'+', ref rest..] => self.tokenize_symbol(1, Symbol::Plus, rest),
-            &[b'-', ref rest..] => self.tokenize_symbol(1, Symbol::Hyphen, rest),
-            &[b'!', ref rest..] => self.tokenize_symbol(1, Symbol::Exclamation, rest),
-            &[b'=', ref rest..] => self.tokenize_symbol(1, Symbol::Equal, rest),
-            &[b'?', ref rest..] => self.tokenize_symbol(1, Symbol::Question, rest),
+                &[b'(', ref rest..] => return self.tokenize_symbol(1, Symbol::OpenParen, rest),
+                &[b')', ref rest..] => return self.tokenize_symbol(1, Symbol::CloseParen, rest),
+                &[b',', ref rest..] => return self.tokenize_symbol(1, Symbol::Comma, rest),
+                &[b'{', ref rest..] => return self.tokenize_symbol(1, Symbol::OpenBrace, rest),
+                &[b'}', ref rest..] => return self.tokenize_symbol(1, Symbol::CloseBrace, rest),
+                &[b'[', ref rest..] => return self.tokenize_symbol(1, Symbol::OpenBracket, rest),
+                &[b']', ref rest..] => return self.tokenize_symbol(1, Symbol::CloseBracket, rest),
+                &[b'|', ref rest..] => return self.tokenize_symbol(1, Symbol::Bar, rest),
+                &[b';', ref rest..] => return self.tokenize_symbol(1, Symbol::Semicolon, rest),
+                &[b':', ref rest..] => return self.tokenize_symbol(1, Symbol::Colon, rest),
+                &[b'#', ref rest..] => return self.tokenize_symbol(1, Symbol::NumberSign, rest),
+                &[b'.', ref rest..] => return self.tokenize_symbol(1, Symbol::Dot, rest),
+                &[b'*', ref rest..] => return self.tokenize_symbol(1, Symbol::Asterisk, rest),
+                &[b'/', ref rest..] => return self.tokenize_symbol(1, Symbol::Slash, rest),
+                &[b'+', ref rest..] => return self.tokenize_symbol(1, Symbol::Plus, rest),
+                &[b'-', ref rest..] => return self.tokenize_symbol(1, Symbol::Hyphen, rest),
+                &[b'<', ref rest..] => return self.tokenize_symbol(1, Symbol::Less, rest),
+                &[b'>', ref rest..] => return self.tokenize_symbol(1, Symbol::Greater, rest),
+                &[b'!', ref rest..] => return self.tokenize_symbol(1, Symbol::Exclamation, rest),
+                &[b'=', ref rest..] => return self.tokenize_symbol(1, Symbol::Equal, rest),
+                &[b'?', ref rest..] => return self.tokenize_symbol(1, Symbol::Question, rest),
+                &[b'\n', ref rest..] => return self.tokenize_symbol(1, Symbol::NewLine, rest),
 
-            // TODO: handle utf8 correctly
-            &[b'$', b'\\', _, ref rest..] => { let position = self.current_position; self.src = rest; self.current_position.forward(3); Some(Ok(Token { text: &self.src[0..1], position, token_type: TokenType::Integer })) },
-            &[b'$', _, ref rest..] => { let position = self.current_position; self.src = rest; self.current_position.forward(2); Some(Ok(Token { text: &self.src[0..1], position, token_type: TokenType::Integer })) },
-
-            &[b'0'..=b'9', ref rest..] => {
-                let mut size: usize = 1;
-                let mut next = rest;
-
-                while let &[b'0'..=b'9', ref rest..] = next {
-                    size += 1;
-                    next = rest;
+                // TODO: handle utf8 correctly
+                &[b'$', b'\\', _, ref rest..] => {
+                    let position = self.current_position;
+                    self.src = rest;
+                    self.current_position.forward(3);
+                    return Some(Ok(Token {
+                        text: &self.src[0..2],
+                        position,
+                        token_type: TokenType::Integer,
+                    }));
+                }
+                &[b'$', _, ref rest..] => {
+                    let position = self.current_position;
+                    self.src = rest;
+                    self.current_position.forward(2);
+                    return Some(Ok(Token {
+                        text: &self.src[0..1],
+                        position,
+                        token_type: TokenType::Integer,
+                    }));
                 }
 
-                match next {
-                    &[b'#', ref rest..] => {
+                &[b'0'..=b'9', ref rest..] => {
+                    let mut size: usize = 1;
+                    let mut next = rest;
+
+                    while let &[b'0'..=b'9', ref rest..] = next {
                         size += 1;
                         next = rest;
+                    }
 
-                        while let &[b'0'..=b'9', ref rest..] = next {
+                    match next {
+                        &[b'#', ref rest..] => {
                             size += 1;
                             next = rest;
+
+                            while let &[b'0'..=b'9', ref rest..] = next {
+                                size += 1;
+                                next = rest;
+                            }
+
+                            let text = &self.src[0..size];
+                            let position = self.current_position;
+
+                            self.src = next;
+                            self.current_position.forward(size);
+
+                            return Some(Ok(Token {
+                                text,
+                                position,
+                                token_type: TokenType::Integer,
+                            }));
                         }
-
-
-                        let text = &self.src[0..size];
-                        let position = self.current_position;
-
-                        self.src = next;
-                        self.current_position.forward(size);
-
-                        Some(Ok(Token { text, position, token_type: TokenType::Integer }))
-                    },
-                    _ => panic!("")
-                }
-            },
-
-            &[b'a'..=b'z', ..] | &[b'_', ..] => {
-                let mut size: usize = 0;
-                let mut next = self.src;
-
-                while let &[b'A'..=b'Z', ref rest..] | &[b'a'..=b'z', ref rest..] | &[b'_', ref rest..] = next {
-                    size += 1;
-                    next = rest;
+                        _ => panic!(""),
+                    }
                 }
 
-                let text = &self.src[0..size];
-                let position = self.current_position;
+                &[b'a'..=b'z', ..] | &[b'_', ..] => {
+                    let mut size: usize = 0;
+                    let mut next = self.src;
 
-                self.src = next;
-                self.current_position.forward(size);
+                    while let &[b'A'..=b'Z', ref rest..] | &[b'a'..=b'z', ref rest..] | &[b'_', ref rest..] = next {
+                        size += 1;
+                        next = rest;
+                    }
 
-                Some(Ok(Token { text, position, token_type: TokenType::Atom }))
+                    let text = &self.src[0..size];
+                    let position = self.current_position;
+
+                    self.src = next;
+                    self.current_position.forward(size);
+
+                    return Some(Ok(Token {
+                        text,
+                        position,
+                        token_type: TokenType::Atom,
+                    }));
+                }
+
+                &[b'A'..=b'Z', ..] => {
+                    let mut size: usize = 0;
+                    let mut next = self.src;
+
+                    while let &[b'A'..=b'Z', ref rest..] | &[b'a'..=b'z', ref rest..] | &[b'_', ref rest..] = next {
+                        size += 1;
+                        next = rest;
+                    }
+
+                    let text = &self.src[0..size];
+                    let position = self.current_position;
+
+                    self.src = next;
+                    self.current_position.forward(size);
+
+                    return Some(Ok(Token {
+                        text,
+                        position,
+                        token_type: TokenType::Variable,
+                    }));
+                }
+
+                &[b' ', ref rest..] | &[b'\t', ref rest..] => {
+                    self.src = rest;
+                    self.current_position.forward(1);
+                    continue;
+                }
+
+                _ => return None,
             }
-
-            &[b'A'..=b'Z', ..] => {
-                let mut size: usize = 0;
-                let mut next = self.src;
-
-                while let &[b'A'..=b'Z', ref rest..] | &[b'a'..=b'z', ref rest..] | &[b'_', ref rest..] = next {
-                    size += 1;
-                    next = rest;
-                }
-
-                let text = &self.src[0..size];
-                let position = self.current_position;
-
-                self.src = next;
-                self.current_position.forward(size);
-
-                Some(Ok(Token { text, position, token_type: TokenType::Variable }))
-            }
-
-            _ => None
         }
     }
+}
+
+#[cfg(test)]
+#[test]
+fn tokenizer_test() {
+    assert_debug_snapshot_matches!(
+        "tokenize symbols",
+        Tokenizer::new(b"( ) , -> { } [ ] | || <- ; : # . \n / + - ++ -- == /= =< < >= \r\n > =:= =/= <= => := << >> ! = :: .. ...")
+            .collect::<Result<Vec<_>, _>>()
+    );
 }
